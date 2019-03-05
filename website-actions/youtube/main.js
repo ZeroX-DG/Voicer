35cf8e8ca9f112f767a832d8e177c7387ec29098
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-03-05 23:36:06 +1300

    added search video for youtube

--- website-actions/youtube/main.js
+++ website-actions/youtube/main.js
@@ -1,20 +1,22 @@
-(function () {
+(function() {
   window.__voicer.addWebActions("youtube", function(action) {
     const $ = window.jQuery;
-    const videoSelector = function () {
+    const searchBoxSelector = "#search input";
+    const searchButtonSelector = "#search-icon-legacy";
+    const videoSelector = function() {
       if (window.location.pathname === "/") {
         return "ytd-grid-video-renderer";
       }
-      return "ytd-compact-video-renderer, ytd-compact-radio-renderer";
-    }
-    const addBorder = function (el) {
+      return "ytd-compact-video-renderer, ytd-compact-radio-renderer, ytd-video-renderer";
+    };
+    const addBorder = function(el) {
       $(el).css("border", "1px solid red");
-    }
-    const removeBorder = function (el) {
+    };
+    const removeBorder = function(el) {
       $(el).css("border", "1px solid transparent");
-    }
+    };
 
-    const handleNextVideo = function () {
+    const handleNextVideo = function() {
       if (!window.__youtube_index) {
         window.__youtube_index = -1;
       }
@@ -25,9 +27,9 @@
         currentVideo = $(videoSelector())[window.__youtube_index];
         addBorder(currentVideo);
       };
-    }
+    };
 
-    const handlePrevVideo = function () {
+    const handlePrevVideo = function() {
       if (!window.__youtube_index) {
         window.__youtube_index = -1;
       }
@@ -41,20 +43,36 @@
         currentVideo = $(videoSelector())[window.__youtube_index];
         addBorder(currentVideo);
       };
-    }
+    };
 
-    const handleGotoVideo = function () {
+    const handleGotoVideo = function() {
       return function() {
-        if (window.__youtube_index === undefined || window.__youtube_index === -1) {
+        if (
+          window.__youtube_index === undefined ||
+          window.__youtube_index === -1
+        ) {
           return;
         }
         const currentVideo = $(videoSelector())[window.__youtube_index];
         removeBorder(currentVideo);
         window.__youtube_index = -1;
         const link = currentVideo.querySelector("a");
-        link.click();
+        // prevent client side redirection
+        window.location.href = link.href;
       };
-    }
+    };
+
+    const handleSearchInput = function() {
+      return function(text) {
+        $(searchBoxSelector).val(text);
+      };
+    };
+
+    const handleSearchGo = function() {
+      return function() {
+        $(searchButtonSelector).trigger("click");
+      };
+    };
 
     if (window.location.hostname == "www.youtube.com") {
       if (action === "next_video") {
@@ -63,6 +81,10 @@
         return handleGotoVideo();
       } else if (action === "prev_video") {
         return handlePrevVideo();
+      } else if (action === "search_input") {
+        return handleSearchInput();
+      } else if (action === "search_go") {
+        return handleSearchGo();
       }
     }
   });
@@ -72,6 +94,8 @@
   window.__voicer.addCommand({
     "next video": webActions["youtube"]("next_video"),
     "previous video": webActions["youtube"]("prev_video"),
+    "enter *text": webActions["youtube"]("search_input"),
+    "find it": webActions["youtube"]("search_go"),
     go: webActions["youtube"]("go")
   });
 })();
